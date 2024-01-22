db6f1b5771948ec945fa7efafd01811d54289d33
Emerson Matos <emegson@live.com>
2024-01-21 22:50:22 -0300

    remove redundant notebooks

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 
-docker run --rm --name colab -v ~/GoogleDrive:/content/drive -p 127.0.0.1:9000:8080 --gpus=all us-docker.pkg.dev/colab-images/public/runtime:latest
+docker run -e "openai-organization=org-jtY20CQ90pWPsQBesjEL1G39" -e "openai-apikey=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" --rm --name colab -v /media/emerson/gdrive:/content/gdrive/MyDrive -p 127.0.0.1:9000:8080 --gpus all us-docker.pkg.dev/colab-images/public/runtime:latest
